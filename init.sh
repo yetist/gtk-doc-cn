e912b88e44b26a42a7f59dda1014cd50f7ae427c
yetist <yetist@8781bae8-c823-0410-986a-3711920aa5e9>
2009-10-09 08:25:09 +0000

    add init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ `id -u` -eq 0 ];then
+	cp tools/gtkdocpo /usr/bin/
+	cp gadt-mkhtml /usr/bin
+else
+	[ -d $HOME/bin ] || mkdir $HOME/bin
+	echo $PATH|grep "~/bin" || $( echo "export PATH=~/bin:$PATH" >> ~/.bashrc; source ~/.bashrc)
+	cp tools/gtkdocpo ~/bin/
+	cp tools/gadt-mkhtml ~/bin
+fi
