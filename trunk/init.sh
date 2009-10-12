886fee73c27d4b058b0aaf664033fe49d190256e
yetist <yetist@8781bae8-c823-0410-986a-3711920aa5e9>
2009-10-12 08:47:27 +0000

    update init.sh

--- trunk/init.sh
+++ trunk/init.sh
@@ -3,9 +3,11 @@
 if [ `id -u` -eq 0 ];then
 	cp tools/gtkdocpo /usr/bin/
 	cp tools/gadt-mkhtml /usr/bin
+	cp tools/autotrans /usr/bin
 else
 	[ -d $HOME/bin ] || mkdir $HOME/bin
 	echo $PATH|grep "~/bin" || $( echo "export PATH=~/bin:$PATH" >> ~/.bashrc; source ~/.bashrc)
 	cp tools/gtkdocpo ~/bin/
 	cp tools/gadt-mkhtml ~/bin
+	cp tools/autotrans ~/bin
 fi
